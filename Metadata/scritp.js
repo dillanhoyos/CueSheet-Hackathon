90650eecb6480b31e206d07bc8884a2b7d0e3b88
dillanhoyos <dillanhoyosmusic@gmail.com>
2022-04-10 02:15:46 -0400

    New Script json
    here audio

--- Metadata/scritp.js
+++ Metadata/scritp.js
@@ -8,9 +8,38 @@
 
         jsmediatags.read (file, {
             onSuccess: function(tag){
-                    console.log(tag);
-                var json = JSON.stringify(tag);
-                   console.log(json);
+                    
+        //    console.log(tag);
+
+        //             console.log(tag.tags.title);
+        //             console.log(tag.tags.artist);
+        //             console.log(tag.tags["©wrt"].data);
+        //             console.log(tag.tags["xid "].data);
+        //             console.log(tag.tags["atID"].data);
+
+           var content = { 
+                "workTitle": tag.tags.title,
+                "iswc": tag.tags["xid "].data,
+                "instrumentVocal": "I",
+                "cueUsageCode": "B",
+                "duration": 32,
+                "sequenceNumber": "A1",
+                "numberOfUses": 3,
+                "cueIps": [
+                {
+                "ipName": tag.tags.artist,
+                "lastName": tag.tags["©wrt"].data,
+                "ipiNameNumber": tag.tags["atID"].data,
+                "ipFunction": "W"
+             }]
+            };
+            console.log(content);
+
+
+                    
+
+
+
                 const data = tag.tags.picture.data;
                 const format = tag.tags.picture.format;
                 let base64String = "";
