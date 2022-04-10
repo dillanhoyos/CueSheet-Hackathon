e36d595e1ff64a3b121898ab8ba5eaf2c3f84a43
Mark Sherriff <63994744+markesherriff@users.noreply.github.com>
2022-04-10 06:00:54 -0400

    update

--- Metadata/scritp.js
+++ Metadata/scritp.js
@@ -8,55 +8,26 @@
 
         jsmediatags.read (file, {
             onSuccess: function(tag){
-                    
-        //    console.log(tag);
 
-        //             console.log(tag.tags.title);
-        //             console.log(tag.tags.artist);
-        //             console.log(tag.tags["©wrt"].data);
-        //             console.log(tag.tags["xid "].data);
-        //             console.log(tag.tags["atID"].data);
+                var content = { 
+                        "workTitle": tag.tags.title,
+                        "iswc": tag.tags["xid "].data,
+                        "instrumentVocal": "I",
+                        "cueUsageCode": "B",
+                        "duration": 32,
+                        "sequenceNumber": "A1",
+                        "numberOfUses": 3,
+                        "cueIps": [
+                        {
+                            "ipName": tag.tags.artist,
+                            "lastName": tag.tags["©wrt"].data,
+                            "ipiNameNumber": tag.tags["atID"].data,
+                            "ipFunction": "W"
+                        }
+                    ]
+                };
+                console.log(content);
 
-           var content = { 
-                "workTitle": tag.tags.title,
-                "iswc": tag.tags["xid "].data,
-                "instrumentVocal": "I",
-                "cueUsageCode": "B",
-                "duration": 32,
-                "sequenceNumber": "A1",
-                "numberOfUses": 3,
-                "cueIps": [
-                {
-                "ipName": tag.tags.artist,
-                "lastName": tag.tags["©wrt"].data,
-                "ipiNameNumber": tag.tags["atID"].data,
-                "ipFunction": "W"
-             }]
-            };
-            console.log(content);
-
-
-                    
-
-
-
-                const data = tag.tags.picture.data;
-                const format = tag.tags.picture.format;
-                let base64String = "";
-
-                // for(let i = 0;i < data.length; i++) 
-                //     base64String += String.fromCharCode(data[i]);
-                        
-
-                     
-                
-                // document.querySelector("#cover").style.backgroundImage = `url(data:${format};base64,${window.btoa(base64String)})`;
-                // document.querySelector("#title").textContent = tag.tags.title; 
-                // document.querySelector("#artist").textContent = tag.tags.artist; 
-                // document.querySelector("#album").textContent = tag.tags.album; 
-                // document.querySelector("#genre").textContent = tag.tags.genre; 
-
-           
             },
             onError: function(error){
                 
---
+++ worker.js
@@ -0,0 +1,156 @@
+const edlFile = document.getElementById('edlFile');
+const audioFile = document.getElementById('audioFile');
+const cueSheetOutput = document.getElementById('cueSheetOutput');
+const createCueSheetButton = document.getElementById('createCueSheetButton');
+
+var audioFilesJson = [];
+var edlJson = [];
+
+function disableCueSheetButton(){
+  if (edlFile.files.length != 0 && audioFile.files.length != 0) createCueSheetButton.removeAttribute('disabled');
+  else createCueSheetButton.disabled = true;
+}
+
+edlFile.addEventListener('change', () => {
+  disableCueSheetButton();
+    let files = edlFile.files;
+
+    if(files.length == 0) return;
+    else {
+      edlJson = parseEDL(files[0]);
+    }
+});
+
+audioFile.addEventListener('change', () => {
+  disableCueSheetButton();
+
+  let files = audioFile.files;
+
+  if (files.length == 0) return;
+  else{
+    audioFilesJson = parseAudioFile(files);
+  }
+});
+
+function parseEDL(edl){
+  const file = edl;
+  let reader = new FileReader();
+
+  reader.onload = (e) =>{
+      const file = e.target.result;
+      const lines = file.split(/\r\n|\n/);
+      var hererString = lines.join('\n');
+      var startString = 'T R A C K  L I S T I N G';
+      var endString = 'M A R K E R S  L I S T I N G';
+    hererString = hererString.substring(
+      hererString.indexOf(startString) + startString.length, 
+      hererString.indexOf(endString)
+      );
+    
+    var trackStringIndicies = [];
+    var lastIndex = 0;
+    while (true){
+      var trackStartIndex = hererString.indexOf("TRACK NAME", lastIndex);
+      if(trackStartIndex == -1){
+        trackStringIndicies.push(hererString.length);
+        break;
+      }
+      trackStringIndicies.push(trackStartIndex);
+      lastIndex = trackStartIndex + 10;
+    }
+    var trackStringList = [];
+    for(var i = 0; i < trackStringIndicies.length-1; i++){
+      trackStringList.push(hererString.substring(trackStringIndicies[i], trackStringIndicies[i+1]));
+    }
+    var clips = [];
+    for(var i = 0; i < trackStringList.length; i++){
+      var clipRawList = trackStringList[i].split('\n');
+      var clipStart = clipRawList.indexOf("CHANNEL \tEVENT   \tCLIP NAME                     \tSTART TIME    \tEND TIME            DURATION      \tTIMESTAMP         \tSTATE");
+      for (var c = clipStart + 1; c < clipRawList.length; c++){
+        if(clipRawList[c] != ""){
+          var clipBreakdown = clipRawList[c].split('\t').map(item => item.trim());
+          if(clipBreakdown[0] == '1'){
+            clipName = clipBreakdown[2];
+            clipName = clipName.split('.L')[0];
+            clipBreakdown = clipBreakdown.slice(3, 6);
+            var timeBreakdown = clipBreakdown[2].split(':');
+            var timeInSecs = (timeBreakdown[0] * 60) + timeBreakdown[1];
+            clips.push({
+              'clipName' : clipName,
+              'numberOfUses' : 1,
+              'duration': parseInt(timeInSecs, 10)
+            });
+          }
+        }
+      }
+      var trackName = trackStringList[i].split('\t');
+      trackName = trackName[1].split('\n');
+    }
+
+    var finalClips = [];
+    for(var i = 0; i < clips.length; i++){
+      var foundIndex = finalClips.map((o) => o.clipName).indexOf(clips[i].clipName);
+      if(foundIndex == -1){
+        finalClips.push(clips[i]);
+        continue;
+      }
+      finalClips[foundIndex]['numberOfUses']++;
+      finalClips[foundIndex]['duration'] += clips[i]['duration'];
+    }
+    return finalClips;
+  };
+  reader.onerror= (e) => alert(e.target.error.name);
+  reader.readAsText(file);
+}
+
+function parseAudioFile(files){
+  var listOfWork = [];
+  const jsmediatags = window.jsmediatags;
+  for(var i = 0; i < files.length; i++){
+    const file = files[i];
+    jsmediatags.read(file, {
+      onSuccess: function (tag) {
+        listOfWork.push({
+          "workTitle": tag.tags.title,
+          "iswc": tag.tags["xid "].data,
+          "instrumentVocal": "I",
+          "cueUsageCode": "B",
+          "duration": 0,
+          "sequenceNumber": "A1",
+          "numberOfUses": 0,
+          "cueIps": [
+            {
+              "ipName": tag.tags.artist,
+              "lastName": tag.tags["©wrt"].data,
+              "ipiNameNumber": tag.tags["atID"].data,
+              "ipFunction": "W"
+            }
+          ]
+        });
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    })
+  }
+  return listOfWork;
+}
+
+function createCueSheet(){
+  mergeEdlAndAudioJson();
+  console.log(audioFilesJson);
+}
+
+function mergeEdlAndAudioJson(){
+  //iteration through audio files to find the edl entry and get duration and numOfUses
+  for(var i = 0; i < audioFilesJson.length; i++){
+    var foundIndex = edlJson.map((o) => o.clipName).indexOf(audioFilesJson[i]["workTitle"]);
+    if(foundIndex != -1){
+      audioFilesJson[i]['duration'] = edlJson[foundIndex]['duration'];
+      audioFilesJson[i]['numberOfUses'] = edlJson[foundIndex]['numberOfUses'];
+    }
+  }
+}
+
+
+
